7b6eb822ec620e77eceded44b34263b2389f6bff
BrowkS <stefanpopescu923@gmail.com>
2020-01-30 12:55:31 +0200

    Changed class methods.

--- src/me/Stefan923/SuperAnnouncer/Utils/Version/Version.java
+++ src/me/Stefan923/SuperAnnouncer/Utils/Version/Version.java
@@ -1,4 +1,13 @@
 package me.Stefan923.SuperAnnouncer.Utils.Version;
 
-public class Version {
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+abstract class Version {
+
+    public abstract void send(final Player player, final String jsonText);
+
+    public abstract void send(final Player player, final List<String> jsonText);
+
 }
